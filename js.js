d6bf9648246c4c9d8a965ea3b7fd9e13ae216ae6
ntv01 <ngotung180301@gmail.com>
2023-02-02 21:18:35 +0700

    update

---
+++ js.js
@@ -0,0 +1,9 @@
+// const handlerDK = document.querySelector(".handlerDK");
+// handlerDK.addEventListener("click", () => {
+//   handlerDK.classList("handlerDN");
+// });
+
+const activedk = document.querySelector(".activedk");
+activedk.addEventListener("click", () => {
+  console.log("ádd");
+});
